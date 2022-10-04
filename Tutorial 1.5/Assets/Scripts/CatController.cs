922d425d96032fe67ee1f50e52db8317914f8478
BuildTools <unconfigured@null.spigotmc.org>
2022-10-03 20:19:01 -0400

    Animation commit

--- Tutorial 1.5/Assets/Scripts/CatController.cs
+++ Tutorial 1.5/Assets/Scripts/CatController.cs
@@ -2,50 +2,84 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CatController : MonoBehaviour
 {
-    public AudioClip musicClipOne;
+  public AudioClip musicClipOne;
 
-    public AudioClip musicClipTwo;
+  public AudioClip musicClipTwo;
 
-    public AudioSource musicSource;
+  public AudioSource musicSource;
 
+  Animator anim;
 
-    void Update()
-    {
+  void Start()
+
+  {
+
+    anim = GetComponent<Animator>();
+
+  }
+void Update()
+{
      if (Input.GetKeyDown(KeyCode.W))
+
         {
+
           musicSource.clip = musicClipOne;
+
           musicSource.Play();
+
+          anim.SetInteger("State", 1);
 
          }
 
      if (Input.GetKeyUp(KeyCode.W))
+
         {
+
           musicSource.Stop();
+
+          anim.SetInteger("State", 0);
 
          }
 
      if (Input.GetKeyDown(KeyCode.R))
+
         {
+
           musicSource.clip = musicClipTwo;
+
           musicSource.Play();
+
+          anim.SetInteger("State", 2);
+
          }
 
      if (Input.GetKeyUp(KeyCode.R))
+
         {
+
           musicSource.Stop();
+
+          anim.SetInteger("State", 0);
 
          }
 
      if (Input.GetKeyDown(KeyCode.L))
+
         {
+
           musicSource.loop = true;
+
          }
 
      if (Input.GetKeyUp(KeyCode.L))
+
         {
+
           musicSource.loop = false;
+
         }
-   }
+
+    }
 }
